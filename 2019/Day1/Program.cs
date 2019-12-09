f142c8e3467fc4202e66131e0ea1375dd858e29d
gnomerd <elalmqvist@gmail.com>
2019-12-09 13:34:35 +0100

    Final commit :)

--- 2019/Day1/Program.cs
+++ 2019/Day1/Program.cs
@@ -10,12 +10,15 @@ namespace _1
 			string[] inputs = input.Split( Environment.NewLine, StringSplitOptions.None ); 
 			int[] output = new int[100];
 			double input_;
+			int sum = 0;
 			for ( int i = 0; i < inputs.Length; i++ ) {
 				Console.WriteLine( inputs[i] );
 				input_ = Convert.ToDouble(inputs[i]);
 				output[i] = (int)Math.Floor( input_/3 ) - 2;
 				Console.WriteLine(output[i]);
+				sum += output[i];
 			}
+			Console.WriteLine("Sum: " + sum.ToString() );
 		}
 	}
 }
