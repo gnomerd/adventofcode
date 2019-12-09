e542c79631b47b8ce2500902277bfa12f1cc254a
gnomerd <elalmqvist@gmail.com>
2019-12-09 14:15:49 +0100

    Improved the program

--- 2019/Day1/Part1/Program.cs
+++ 2019/Day1/Part1/Program.cs
@@ -1,24 +1,36 @@
 ﻿using System;
 using System.IO;
 
-namespace _1
-{
-    class Program {
-		static void Main(string[] args) {
-			string input = File.ReadAllText(@"../../input.txt");
+namespace _1 {
 
-			string[] inputs = input.Split( Environment.NewLine, StringSplitOptions.None ); 
+	public class Part1 {
+		public int[] GetFuelModules( string[] inputs ) {
 			int[] output = new int[100];
 			double input_;
-			int sum = 0;
 			for ( int i = 0; i < inputs.Length; i++ ) {
-				Console.WriteLine( inputs[i] );
 				input_ = Convert.ToDouble(inputs[i]);
-				output[i] = (int)Math.Floor( input_/3 ) - 2;
+				output[i] = (int)Math.Floor( input_/3 ) - 2; // do the fuel calc
 				Console.WriteLine(output[i]);
-				sum += output[i];
 			}
-			Console.WriteLine("Sum: " + sum.ToString() );
+			return output;
+		}
+	}
+    class Program {
+
+		
+		static void Main(string[] args) {
+			string input = File.ReadAllText(@"../../input.txt"); // get the input
+
+			string[] inputs = input.Split( Environment.NewLine, StringSplitOptions.None ); 
+			int[] modules = new int[100];
+
+			Part1 prt1 = new Part1();
+			modules = prt1.GetFuelModules( inputs ); // get the fuel for the mass'es
+
+			int sum = 0;
+			for( int i = 0; i < modules.Length; i++ ) { sum += modules[i]; } // add all of the fuel
+
+			Console.WriteLine("Sum: " + sum.ToString() ); //output the answer
 		}
 	}
 }
