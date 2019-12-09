041ad2fe5fa7f6be0cacf3e9d2d14a40a4669a1a
gnomerd <elalmqvist@gmail.com>
2019-12-09 11:11:50 +0100

    Initial commit

---
+++ 2019/1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.IO;
+
+namespace _1
+{
+    class Program {
+		static void Main(string[] args) {
+			string input = System.IO.File.ReadAllText(@"./input.txt");
+			Console.Write(input);
+		}
+	}
+}
