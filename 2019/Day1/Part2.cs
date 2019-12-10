e72b6d3b6dc1233adf4c69cd2f98a087c95229a4
gnomerd <elalmqvist@gmail.com>
2019-12-10 21:58:25 +0100

    Stuff

--- 2019/Day1/Part2.cs
+++ 2019/Day1/Part2.cs
@@ -2,5 +2,13 @@
 using System.IO;
 
 namespace Part2 {
-	
+	public class Get {
+        
+        public int GetTotalFuel( int[] modules ) {
+            for( int i = 0; i < modules.Length; i++ ) {
+
+            }
+            return 0;
+        }
+    }
 }
