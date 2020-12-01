daad3c4a06b74a33e3c0fe03f95c0b2a2f941e38
E. Almqvist <elalmqvist@gmail.com>
2020-12-01 06:21:53 +0100

    Day 1

---
+++ 2020/1/solution.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+
+
+def getNums(nums):
+    numsLen = len(nums)
+
+    for i in range(numsLen):
+        for j in range(numsLen):
+            if( nums[i] + nums[j] == 2020 ):
+                return nums[i], nums[j]
+
+    print("None found :(")
+    return
+
+
+def getNumsPart2(nums):
+    numsLen = len(nums)
+    for i in range(numsLen):
+        for j in range(numsLen):
+            for k in range(numsLen):
+                if( nums[i] + nums[j] + nums[k] == 2020 ):
+                    return nums[i], nums[j], nums[k]
+
+    print("None found")
+
+inp = open("input", "r")
+nums = inp.read().splitlines()
+
+nums = list(map(int, nums))
+print(nums)
+
+
+# PART 1
+x, y = getNums(nums)
+theNum = x * y
+print( f"1; OUTPUT: {getNums(nums)} | theNum: {theNum}" )
+
+
+# PART 2
+x, y, z = getNumsPart2(nums)
+theNum = x * y * z
+print(theNum)
