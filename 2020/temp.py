ac66207e0206bc39beb00db4e97b2532a5863607
E. Almqvist <elalmqvist@gmail.com>
2020-12-06 19:37:07 +0100

    Template

---
+++ 2020/temp.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+
+from aoc import get_input # AoC
+import re # regex
+
+data = get_input(DAY)
+
--- 2020/test.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-from aoc import get_input
-data = get_input(1).splitlines()
-
-print(data)
